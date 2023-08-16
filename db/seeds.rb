8fe584d8b721ffffef755baca4d82f352d346eba
herve hublet <hervehublet@hotmail.com>
2023-08-16 11:51:50 +0200

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+resto1 = Restaurant.create(name: 'resto1', address: 'address1', phone_number: 'phone1', category: 'chinese')
+resto2 = Restaurant.create(name: 'resto2', address: 'address2', phone_number: 'phone2', category: 'italian')
+resto3 = Restaurant.create(name: 'resto3', address: 'address3', phone_number: 'phone3', category: 'japanese')
+resto4 = Restaurant.create(name: 'resto4', address: 'address4', phone_number: 'phone4', category: 'french')
+resto5 = Restaurant.create(name: 'resto5', address: 'address5', phone_number: 'phone5', category: 'belgian')
